a02c77ac0484f886628a601127d8de765310fca4
Azaniyah Shephard <session1@START-Coding-MBP-C02PP4G9G8WN.local>
2015-10-08 14:15:10 -0700

    Adding in the base

--- animals.js
+++ animals.js
@@ -1,3 +1,33 @@
 /**
  * Created by session1 on 10/7/15.
  */
+google.setOnLoadCallback(drawStuff);
+
+function drawStuff() {
+    var data = new google.visualization.arrayToDataTable([
+        ['Opening Move', 'Percentage'],
+        ["King's pawn (e4)", 44],
+        ["Queen's pawn (d4)", 31],
+        ["Knight to King 3 (Nf3)", 12],
+        ["Queen's bishop pawn (c4)", 10],
+        ['Other', 3]
+    ]);
+
+    var options = {
+        title: 'Chess opening moves',
+        width: 900,
+        legend: { position: 'none' },
+        chart: { title: 'Chess opening moves',
+            subtitle: 'popularity by percentage' },
+        bars: 'horizontal', // Required for Material Bar Charts.
+        axes: {
+            x: {
+                0: { side: 'top', label: 'Percentage'} // Top x-axis.
+            }
+        },
+        bar: { groupWidth: "90%" }
+    };
+
+    var chart = new google.charts.Bar(document.getElementById('top_x_div'));
+    chart.draw(data, options);
+};
